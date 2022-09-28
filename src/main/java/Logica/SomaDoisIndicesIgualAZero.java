f8b5e883e18e623bcc4fca6db00452bacd5e06fd
brenobcsilva <breno.csilva@hotmail.com>
2022-09-28 10:02:13 -0300

    Encontrar soma de dois indices com resultado diferente de zero, criando Set static

--- src/main/java/Logica/SomaDoisIndicesIgualAZero.java
+++ src/main/java/Logica/SomaDoisIndicesIgualAZero.java
@@ -1,4 +1,37 @@
 package Logica;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SomaDoisIndicesIgualAZero {
+
+    static Set<Integer> set = new HashSet<>();
+
+    public static Boolean hasZeroSumSubarray(int[] nums){
+
+        set.add(0);
+        int sum=0;
+
+        for(int value: nums){
+            sum += value;
+
+            if(set.contains(sum)){
+                return true;
+            }
+            set.add(sum);
+        }
+
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 3,-3 };
+
+        if(hasZeroSumSubarray(nums)){
+            System.out.println("Subarray exists");
+        }else{
+            System.out.println("Subarray does not exist");
+        }
+    }
 }
